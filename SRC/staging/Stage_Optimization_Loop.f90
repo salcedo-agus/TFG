df128d442e707a14c54651c24a5b9dff04b36d00
Facu Sosa <fsosa180@alumnos.iua.edu.ar>
2026-07-07 19:30:24 -0300

    Finished subroutine DV_loss

--- SRC/staging/Stage_Optimization_Loop.f90
+++ SRC/staging/Stage_Optimization_Loop.f90
@@ -2,26 +2,34 @@ subroutine STAGING_LOOP(Rocket)
     use typical_data
     use rocket_types
     use constants
-    !ACA IRIA MODULO CON DATOS DE LA ORBITA, R, H, V_CIRC
     implicit none
     type(Rocket_t), intent(inout) :: Rocket
     integer i
     real(8) DV_old, DV_new !old and new Delta_v iterations
     real(8) err            !difference between 2 iterations of Delta_v
-    real(8) V_or, V_circ           !V* en LaunchMethodology
+    real(8) V_ast, V_circ           !V* en LaunchMethodology
     real(8) Ta                     !Tiempo de ascenso directo
 
-    V_or = sqrt(V_circ**2.d0 + 2.d0*g*H*(R/(R+H))**2.d0)
-    DV_old = V_or + 1.5e-3*Ta**2.d0 + 8.82e-2*Ta + 1036.d0
+    V_circ = delta_v
+    V_ast = sqrt(V_circ**2.d0 + 2.d0*g_0*orbit_height*(Radius/(Radius+orbit_height))**2.d0)
+    DV_old = V_ast + 1.5e-3*Ta**2.d0 + 8.82e-2*Ta + 1036.d0
+
     do while(err < 1e-3)
 
         call STAGING(Rocket)    
+
         call DV_loss(Rocket,Ta,DV_new)   
+        
         err = abs(DV_old - DV_new)
+
+        DV_old=DV_new
+        print*, DV_new
+
     end do
+
 end subroutine STAGING_LOOP
 
-subroutine DV_loss(Rocket,Ta)
+subroutine DV_loss(Rocket,Ta,DV_new)
     use typical_data
     use rocket_types
     use constants
@@ -30,28 +38,32 @@ subroutine DV_loss(Rocket,Ta)
     implicit none
     type(Rocket_t), intent(inout) :: Rocket
     integer i
-    real(8) K1, K2, K3, K4, DV_loss1, DV_loss2, DV_new, V_rot, V_circ
-    real(8) Hp, Ta
+    real(8) K1, K2,  DV_loss1           !VARIABLES PARA EL CALCULO DE DV_loss1
+    real(8) K3, K4, DV_loss2, T_3s, ISP, A0, T_mix, expo      !VARIABLES PARA EL CALCULO DE DV_loss2
+    real(8) Ta, DV_old, DV_new, V_rot, V_circ       !VARIABLES COMUNES PARA AMBOS CALCULOS
     real(8) err            !difference between 2 iterations of Delta_v
 
-    K1 = 662.1d0 + 1.602d0*Hp + 1.224e-3*Hp**2.d0
+    V_circ=delta_v
+
+  !  K1 = 662.1d0 + 1.602d0*orbit_height + 1.224e-3*orbit_height**2.d0
     
-    K2 = 1.7871 - 9.687e-4*Hp
-
- !   K3 = 429.9d0 + 1.602d0*Hp + 1.224e-3*Hp**2.d0
-
-  !  K4 = 2.328d0 - 9.687e-4*Hp
+ !   K2 = 1.7871 - 9.687e-4*orbit_height
+ 
+  !  DV_loss1 = K1 + K2*Ta
     
-    DV_loss1 = K1 + K2*Ta
+    K3 = 429.9d0 + 1.602d0*orbit_height + 1.224e-3*orbit_height**2.d0
 
-    expo = -0.333d0*DV_loss/(g*ISP)
+    K4 = 2.328d0 - 9.687e-4*orbit_height
+   
 
-    T_3s = 3*(1-exp(expo))*g*ISP/A0
+    expo = -0.333d0*DV_loss2/(g_0*ISP)
+
+    T_3s = 3*(1-exp(expo))*g_0*ISP/A0
 
     T_mix = 0.405d0*Ta + 0.595d0*T_3s
 
-   ! DV_loss2 = K3 + K4*T_mix
+    DV_loss2 = K3 + K4*T_mix
 
-    DV_new = V_circ + DV_loss - V_rot
+    DV_new = V_circ + DV_loss2 - V_rot      !V_circ + DV_loss1 - V_rot si usamos el metodo sin refinar
 
 end subroutine DV_loss
